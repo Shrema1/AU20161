8e49fdde4d3063d3d4a339151ff8279fe50b4506
Shrema Singh <shrema.singh@accoliteindia.com>
2016-07-07 11:30:32 +0530

    I am Commiting

---
+++ demoProject/src/demoProject/demoClass.java
@@ -0,0 +1,62 @@
+package demoProject;
+
+import java.util.Scanner;
+
+public class demoClass implements interFace {
+
+	@Override
+	public Integer add(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return (a+b);
+	}
+
+	@Override
+	public Integer subtract(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return (a-b);
+	}
+
+	@Override
+	public Integer divide(Integer a, Integer b) throws ArithmeticException {
+		// TODO Auto-generated method stub
+		return (a/b);
+	}
+
+	@Override
+	public Integer multiply(Integer a, Integer b) {
+		// TODO Auto-generated method stub
+		return (a*b);
+	}
+public static void main(String[]args)
+{
+	demoClass obj=new demoClass();
+	Integer a,b,choice;
+	Scanner input=new Scanner(System.in);
+	System.out.println("Enter first number");
+	a=input.nextInt();
+	System.out.println("Enter second number");
+	b=input.nextInt();
+	System.out.println("Enter your selection:\n 1)Add \n 2)Subtract \n 3)Divide \n 4)Multiply\n");
+	choice=input.nextInt();
+	switch(choice){
+	case 1:System.out.println(obj.add(a,b));
+	break;
+	case 2:System.out.println(obj.subtract(a,b));
+	break;
+	case 3:
+		try
+		{
+			System.out.println(obj.divide(a,b));
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Cant divide by zero");
+		}
+	break;
+	case 4:System.out.println(obj.multiply(a,b));
+	break;
+	default:System.out.println("Invalid choice");
+	}
+}
+
+}
---
+++ demoProject/src/demoProject/interFace.java
@@ -0,0 +1,10 @@
+package demoProject;
+
+public interface interFace
+{
+public Integer add(Integer a,Integer b);
+public Integer subtract(Integer a,Integer b);
+public Integer divide(Integer a,Integer b)throws ArithmeticException;
+public Integer multiply(Integer a,Integer b);
+	
+}
